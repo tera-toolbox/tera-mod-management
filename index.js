957a5f8e5e0f1a277568a55fae4727b36eeaed36
Caali <modsfortera@gmail.com>
2020-02-27 01:51:26 +0100

    Various updates.

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ const CoreModules = {
 };
 
 // Installed module management
+function forcedirSync(dir) {
+    const sep = path.sep;
+    const initDir = path.isAbsolute(dir) ? sep : '';
+    dir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = path.resolve(parentDir, childDir);
+        try {
+            fs.mkdirSync(curDir);
+        } catch (_) {
+            // Ignore
+        }
+
+        return curDir;
+    }, initDir);
+}
+
 function installModule(rootFolder, installInfo, nameOverride = null) {
     const modName = nameOverride || installInfo.name;
     const modFolder = path.join(rootFolder, modName);
-    fs.mkdirSync(modFolder);
+    forcedirSync(modFolder);
     fs.writeFileSync(path.join(modFolder, 'module.json'), JSON.stringify(installInfo, null, 4));
 }
 
@@ -29,10 +44,10 @@ function uninstallModule(moduleInfo) {
         fs.rmdirSync(dir_path);
     }
 
-    if (moduleInfo.type === 'standalone')
-        fs.unlinkSync(moduleInfo.path);
-    else
+    if (fs.lstatSync(moduleInfo.path).isDirectory())
         rmdirSyncForce(moduleInfo.path);
+    else
+        fs.unlinkSync(moduleInfo.path);
 }
 
 function isCoreModule(moduleInfo) {
@@ -41,11 +56,13 @@ function isCoreModule(moduleInfo) {
 
 function listModules(rootFolder) {
     let names = [];
+    if (!fs.existsSync(rootFolder))
+        return;
 
     for (let name of fs.readdirSync(rootFolder)) {
         if (name[0] === "." || name[0] === "_")
             continue;
-        if (!name.endsWith(".js") && !fs.lstatSync(path.join(rootFolder, name)).isDirectory())
+        if (!name.endsWith(".js") && !fs.statSync(path.join(rootFolder, name)).isDirectory())
             continue;
 
         names.push(name);
@@ -86,7 +103,7 @@ function loadModuleInfo(rootFolder, name) {
         'packets': {},
     };
 
-    const standalone = !fs.lstatSync(modulePath).isDirectory();
+    const standalone = !fs.statSync(modulePath).isDirectory();
     if (standalone) {
         if (!name.endsWith(".js"))
             throw new Error(`Invalid mod ${name}`);
